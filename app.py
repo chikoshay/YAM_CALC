945a5d641b33d8294a3b91a8e15bbecfacee5d64
Shay Chikotay <chikoshay@gmail.com>
2020-10-27 18:52:15 +0200

    First commit

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def YAM():
+    fake_time = [999]
+    status = "Yam's Python course is 16 weeks long..."
+    if request.method == "GET":
+        if len(request.args.getlist("weeks")) == 0:
+            verdict = "What week are you in?"
+            holishit_result = ""
+        else:
+            fake_time = request.args.getlist("weeks")
+            fake_time = int(fake_time[0])
+            time_left = (16 - fake_time) * 4
+            verdict = f"your are at week {fake_time}"
+            holishit_result = f"that means you have {time_left} weeks to go!"
+    return render_template("index.html",
+                           status=status,
+                           verdict=verdict,
+                           holishit_result=holishit_result
+                           )
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
